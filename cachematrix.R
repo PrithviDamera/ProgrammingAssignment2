2bd496c22350c2e0c1018d5b92acdace1f13e387
PrithviDamera <dameraprithvi369@gmail.com>
2015-04-26 00:05:27 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
 ## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(pb = matrix()) {
+        ##             Will use the Set and get fn’s for  the matrix inversed
+        inv = NULL
+        set = function(y) {
+                pb <<- m
+                inv <<- NULL
+        }
+        get = function() pb
+        setinv = function(inverse) inv <<- inverse 
+        getinv = function() inv
+        list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+cacheSolve <- function(pb, ...) {
+                ## Inverse of the matrix will be returned
+        inv = pb$getinv()
+        if (!is.null(inv)){
+                message("getting cached data")
+                return(inv)
+        }
+        # In the above method, it calculates the inverse, if not here it calculates the inverse 
+        mat.data = pb$get()
+        inv = solve(mat.data, ...)
+        pb$setinv(inv)
+        
+        return(inv)
+}
+test = function(mat){
+        ## mat variable helps in the invertible matrix
+        temp = makeCacheMatrix(mat)
+        start.time = Sys.time()
+        cacheSolve(temp)
+        dur = Sys.time() - start.time
+        print(dur)
+        start.time = Sys.time()
+        cacheSolve(temp)
+        dur = Sys.time() - start.time
+        print(dur)
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
